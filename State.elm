dc41e87031508c000a323cfa2e521a57885ce586
Tom Oram <tom@codept.com>
2018-01-04 22:34:15 +0000

    Extract Builder state updates to Builder.State

--- State.elm
+++ State.elm
@@ -3,37 +3,20 @@ module State exposing (initModel, update)
 import Pizza exposing (Pizza)
 import Types exposing (Model, Msg(..))
 import List.Extra
-import Builder.Types
+import Builder.State
 import Cart.Types
 
 
 initModel : ( Model, Cmd Msg )
 initModel =
-    ( { newPizza = Nothing, cart = [] }, Cmd.none )
+    ( { newPizza = Builder.State.initModel, cart = [] }, Cmd.none )
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
         BuilderMsg builderMsg ->
-            case builderMsg of
-                Builder.Types.SelectBase base ->
-                    ( { model | newPizza = Just (Pizza.new base) }, Cmd.none )
-
-                Builder.Types.Cancel ->
-                    ( { model | newPizza = Nothing }, Cmd.none )
-
-                Builder.Types.ChangeBase base ->
-                    ( updateNewPizza (Pizza.setBase base) model, Cmd.none )
-
-                Builder.Types.AddTopping topping ->
-                    ( updateNewPizza (Pizza.addTopping topping) model, Cmd.none )
-
-                Builder.Types.RemoveTopping topping ->
-                    ( updateNewPizza (Pizza.removeTopping topping) model, Cmd.none )
-
-                Builder.Types.ResetToppings ->
-                    ( updateNewPizza Pizza.resetToppings model, Cmd.none )
+            ( { model | newPizza = Builder.State.update builderMsg model.newPizza }, Cmd.none )
 
         CartMsg cartMsg ->
             case cartMsg of
