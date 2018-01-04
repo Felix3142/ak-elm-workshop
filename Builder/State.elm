b771c8546a460e3371c8cf5a422d1cc4e5eb00c7
Tom Oram <tom@codept.com>
2018-01-04 22:34:15 +0000

    Extract Builder state updates to Builder.State

---
+++ Builder/State.elm
@@ -0,0 +1,31 @@
+module Builder.State exposing (initModel, update)
+
+import Builder.Types exposing (Msg(..))
+import Pizza exposing (Pizza)
+
+
+initModel : Maybe Pizza
+initModel =
+    Nothing
+
+
+update : Builder.Types.Msg -> Maybe Pizza -> Maybe Pizza
+update msg model =
+    case msg of
+        SelectBase base ->
+            Just (Pizza.new base)
+
+        Cancel ->
+            Nothing
+
+        ChangeBase base ->
+            Maybe.map (Pizza.setBase base) model
+
+        AddTopping topping ->
+            Maybe.map (Pizza.addTopping topping) model
+
+        RemoveTopping topping ->
+            Maybe.map (Pizza.removeTopping topping) model
+
+        ResetToppings ->
+            Maybe.map Pizza.resetToppings model
